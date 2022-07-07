90355ae3da40bd2fe4e42bc66fa5a9c17bc8f1ae
ThomasDang93 <thomasdang93@gmail.com>
2022-07-07 00:09:27 -0500

    remove crust

--- src/components/LetterPlanting.js
+++ src/components/LetterPlanting.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { create, IPFSHTTPClient } from 'ipfs-http-client';
 import { ethers } from 'ethers';
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { typesBundleForPolkadot } from '@crustio/type-definitions';
+//import { typesBundleForPolkadot } from '@crustio/type-definitions';
 import { Keyring } from '@polkadot/keyring';
 import { Buffer } from 'buffer';
 import './components.css';
@@ -10,10 +10,10 @@ import LetterBoxingABI from "./LetterBoxing.json";
 const crustChainEndpoint = 'wss://rpc.crust.network'; // More endpoints: https://github.com/crustio/crust-apps/blob/master/packages/apps-config/src/endpoints/production.ts#L9
 const ipfsW3GW = 'https://crustipfs.xyz'; // More web3 authed gateways: https://github.com/crustio/ipfsscan/blob/main/lib/constans.ts#L29
 const crustSeeds = process.env.REACT_APP_CRUST_SEED; //process.env.CRUST_SEED; // Create account(seeds): https://wiki.crust.network/docs/en/crustAccount
-const api = new ApiPromise({
-    provider: new WsProvider(crustChainEndpoint),
-    typesBundle: typesBundleForPolkadot,
-});
+// const api = new ApiPromise({
+//     provider: new WsProvider(crustChainEndpoint),
+//     typesBundle: typesBundleForPolkadot,
+// });
 const DEPLOYED_CONTRACT_ADDRESS = '0x1D467E6201DbB81a91f2Cc46186bD382DE7a0F68';
 
 function LetterPlanting() {
@@ -78,15 +78,15 @@ function LetterPlanting() {
             const mdf = await addFile(ipfsRemote, JSON.stringify(metaData)); // Or use IPFS local
             console.log(mdf);
             // Place storage order
-            await placeStorageOrder(rst.cid, rst.size);
+            // await placeStorageOrder(rst.cid, rst.size);
 
-            // Query storage status
-            // Query forever here ...
-            while (true) {
-                const orderStatus = (await getOrderState(rst.cid)).toJSON();
-                console.log('Replica count: ', orderStatus['reported_replica_count']); // Print the replica count
-                await new Promise(f => setTimeout(f, 1500)); // Just wait 1.5s for next chain-query
-            }
+            // // Query storage status
+            // // Query forever here ...
+            // while (true) {
+            //     const orderStatus = (await getOrderState(rst.cid)).toJSON();
+            //     console.log('Replica count: ', orderStatus['reported_replica_count']); // Print the replica count
+            //     await new Promise(f => setTimeout(f, 1500)); // Just wait 1.5s for next chain-query
+            // }
 
         } else {
             alert("Please enter value for all mandatory fields");
@@ -147,42 +147,42 @@ function LetterPlanting() {
         
     }
 
-    async function placeStorageOrder(fileCid, fileSize) {
-        // 1. Construct place-storage-order tx
-        const tips = 0;
-        const memo = '';
-        const tx = api.tx.market.placeStorageOrder(fileCid, fileSize, tips, memo);
-        console.log(tx)
-        // 2. Load seeds(account)
-        const kr = new Keyring({ type: 'sr25519' });
-        const krp = kr.addFromUri(crustSeeds);
-        console.log(krp)
-        // 3. Send transaction
-        await api.isReadyOrError;
-        return new Promise((resolve, reject) => {
-            tx.signAndSend(krp, ({events = [], status}) => {
-                console.log(`💸  Tx status: ${status.type}, nonce: ${tx.nonce}`);
+    // async function placeStorageOrder(fileCid, fileSize) {
+    //     // 1. Construct place-storage-order tx
+    //     const tips = 0;
+    //     const memo = '';
+    //     const tx = api.tx.market.placeStorageOrder(fileCid, fileSize, tips, memo);
+    //     console.log(tx)
+    //     // 2. Load seeds(account)
+    //     const kr = new Keyring({ type: 'sr25519' });
+    //     const krp = kr.addFromUri(crustSeeds);
+    //     console.log(krp)
+    //     // 3. Send transaction
+    //     await api.isReadyOrError;
+    //     return new Promise((resolve, reject) => {
+    //         tx.signAndSend(krp, ({events = [], status}) => {
+    //             console.log(`💸  Tx status: ${status.type}, nonce: ${tx.nonce}`);
     
-                if (status.isInBlock) {
-                    events.forEach(({event: {method, section}}) => {
-                        if (method === 'ExtrinsicSuccess') {
-                            console.log(`✅  Place storage order success!`);
-                            resolve(true);
-                        }
-                    });
-                } else {
-                    // Pass it
-                }
-            }).catch(e => {
-                reject(e);
-            })
-        });
-    }
+    //             if (status.isInBlock) {
+    //                 events.forEach(({event: {method, section}}) => {
+    //                     if (method === 'ExtrinsicSuccess') {
+    //                         console.log(`✅  Place storage order success!`);
+    //                         resolve(true);
+    //                     }
+    //                 });
+    //             } else {
+    //                 // Pass it
+    //             }
+    //         }).catch(e => {
+    //             reject(e);
+    //         })
+    //     });
+    // }
 
-    async function getOrderState(cid) {
-        await api.isReadyOrError;
-        return await api.query.market.filesV2(cid);
-    }
+    // async function getOrderState(cid) {
+    //     await api.isReadyOrError;
+    //     return await api.query.market.filesV2(cid);
+    // }
 
     function renderMetamask() {
         if (!state.selectedAddress) {
