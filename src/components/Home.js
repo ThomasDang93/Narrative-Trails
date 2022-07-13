93993d5adfcd85cb309fc7d60f37f4d17e4c95fa
ThomasDang93 <thomasdang93@gmail.com>
2022-07-12 22:34:58 -0500

    add connect button to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,60 @@
 import logo from './logo.png';
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import './components.css';
+import LetterBoxingABI from "./LetterBoxing.json";
+import MyStamp from "./MyStamp";
+import * as  constants from './constants.js';
 
+export const injected = new InjectedConnector();
 function Home() {
+  const [hasMetamask, setHasMetamask] = useState(false);
+    const {
+        active,
+        activate,
+        account,
+        library: provider,
+      } = useWeb3React();
+      async function connect() {
+        if (typeof window.ethereum !== "undefined") {
+          try {
+            await activate(injected);
+            setHasMetamask(true);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      }
+
+      useEffect(() => {
+        if (typeof window.ethereum !== "undefined") {
+          setHasMetamask(true);
+        }
+      });
+
+      {hasMetamask ? (
+        active ? (
+        <div className="top-right">Connected</div>
+        ) : (
+            <button className="top-right" onClick={() => connect()}>Connect</button>
+        )
+    ) : (
+        <div className="top-right">Please Install Metamask</div>
+    )}
 
     return (
         <div className="App">
+          {hasMetamask ? (
+                active ? (
+                <div className="top-right">Connected</div>
+                ) : (
+                    <button className="top-right" onClick={() => connect()}>Connect</button>
+                )
+            ) : (
+                <div className="top-right">Please Install Metamask</div>
+            )}
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <a
