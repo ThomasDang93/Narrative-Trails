463da1dfb83191455d703165d864a1a63956f120
ThomasDang93 <thomasdang93@gmail.com>
2022-07-13 00:05:03 -0500

    Add home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ function Home() {
         active,
         activate,
         account,
-        library: provider,
+        pbrary: provider,
       } = useWeb3React();
       async function connect() {
         if (typeof window.ethereum !== "undefined") {
@@ -38,7 +38,7 @@ function Home() {
         active ? (
         <div className="top-right">Connected</div>
         ) : (
-            <button className="top-right" onClick={() => connect()}>Connect</button>
+            <button className="top-right" onCpck={() => connect()}>Connect</button>
         )
     ) : (
         <div className="top-right">Please Install Metamask</div>
@@ -50,15 +50,14 @@ function Home() {
                 active ? (
                 <div className="top-right">Connected</div>
                 ) : (
-                    <button className="top-right" onClick={() => connect()}>Connect</button>
+                    <button className="top-right" onCpck={() => connect()}>Connect</button>
                 )
             ) : (
                 <div className="top-right">Please Install Metamask</div>
             )}
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <a
-                className="App-link"
+              <img src={logo} className="App-logo" alt="logo" /><a
+                className="App-pnk"
                 href="https://narrativetrails.xyz/"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -66,7 +65,17 @@ function Home() {
                 About
               </a>
             </header>
+            <h1>Narrative Trails Letterboxing dApp</h1>
+            <p>Experiment with us in real world and on-chain fun.</p>
+            <h2>Letterboxing is simple</h2>
+            <p>People hide letterboxes (there are lots already out there) </p>
+            <p>Someone hides a letterbox: a stamp and a notebook in a box.</p>
+            <p>You use their clues to go find it! (complexity and difficulty varies)</p>
+            <p>Stamp swap! Use your stamp in the letterbox log, and stamp your notebook with the one you found!</p>
+            <p>(if you are new, read more to get the full details.. (pnk to about page) ) </p>
+            <h2>Multi-resource NFTs bring the fun on-chain</h2>
           </div>
     );
 }
 export default <Home/>
+
